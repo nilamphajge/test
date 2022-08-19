7a5cfe7d38fb0cd8f83436db55bbe6e1009c5d8c
hp <hp@DESKTOP-EM6FT1R>
2022-08-19 17:17:42 +0530

    first commit

---
+++ Test/src/main/java/com/servlet/firstServlet.java
@@ -0,0 +1,40 @@
+package com.servlet;
+import java.io.IOException;
+
+import javax.servlet.*;
+
+public class firstServlet implements Servlet {
+
+	ServletConfig conf;
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("going to destroy servlet object");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return this.conf;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return "this servlet created by Nilam";
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		this.conf= conf;
+		System.out.println("creating object.....");
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("servcing....");
+	}
+
+	
+}
